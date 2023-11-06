0ef0323a5ac302b3672c6faab518358c37839c69
luckyrabbit85 <amanroland@gmail.com>
2023-11-06 10:10:40 +0530

    addded slack handler to app logger

--- flask_app/utilities/app_logger.py
+++ flask_app/utilities/app_logger.py
@@ -1,8 +1,22 @@
+import json
 import logging
 from logging.config import dictConfig
+import requests
+
+
+# for sending error logs to slack
+class HTTPSlackHandler(logging.Handler):
+    def emit(self, record):
+        log_entry = self.format(record)
+        json_text = json.dumps({"text": log_entry})
+        url = "https://hooks.slack.com/services/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"
+        return requests.post(
+            url, json_text, headers={"Content-type": "application/json"}
+        ).content
 
 
 def setup_app_logger(name, debug=True):
+    logging.HTTPSlackHandler = HTTPSlackHandler
     dictConfig(
         {
             "version": 1,
@@ -20,6 +34,11 @@ def setup_app_logger(name, debug=True):
                     "class": "logging.StreamHandler",
                     "formatter": "default",
                     "stream": "ext://sys.stdout",
+                },
+                "slack": {
+                    "class": "logging.HTTPSlackHandler",
+                    "formatter": "default",
+                    "level": "ERROR",
                 },
             },
             "loggers": {
