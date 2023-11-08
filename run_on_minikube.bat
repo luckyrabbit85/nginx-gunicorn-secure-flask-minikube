7f477d3565a847033f9a73525b156a7eea2e7434
luckyrabbit85 <amanroland@gmail.com>
2023-11-08 15:28:02 +0530

    added minikube tunnel to expose ip to program

--- run_on_minikube.bat
+++ run_on_minikube.bat
@@ -2,22 +2,25 @@
 REM Start Minikube
 minikube start
 
-REM Create ConfigMap
+REM Expose the external IP directly to any program running on the host operating system
+start cmd /k "minikube tunnel"
+
+REM Create ConfigMap for Nginx configuration
 kubectl create configmap nginx-config-map --from-file=nginx-config.conf
 
-REM Create TLS Secret
+REM Create TLS Secret for Nginx using provided certificates
 kubectl create secret tls tls-certs-secret --cert=nginx-server.crt --key=nginx-server.key
 
-REM Deploy Manifests
+REM Deploy Kubernetes Manifests for Iris app, services, and Nginx
 kubectl apply -f iris-app.yaml -f iris-service.yaml -f nginx.yaml -f nginx-service.yaml
 
-REM Wait for the resources to become available
+REM Wait for resources to become available
 echo Waiting for Iris deployment...
-kubectl wait --for=condition=Available --timeout=60s deployment.apps/iris
+kubectl wait --for=condition=Available --timeout=90s deployment.apps/iris
 
 echo Waiting for Nginx deployment...
-kubectl wait --for=condition=Available --timeout=60s deployment.apps/nginx
+kubectl wait --for=condition=Available --timeout=90s deployment.apps/nginx
 
-REM Check services
+REM Display services available in the cluster
 echo Services in the cluster:
 kubectl get services
