dc1a0878a92bbce70ade7b161c27243c247c13d6
luckyrabbit85 <amanroland@gmail.com>
2023-11-08 18:33:43 +0530

    yaml & config files to k8s and renamed paths

--- run_on_minikube.bat
+++ run_on_minikube.bat
@@ -6,13 +6,13 @@ REM Expose the external IP directly to any program running on the host operating
 start cmd /k "minikube tunnel"
 
 REM Create ConfigMap for Nginx configuration
-kubectl create configmap nginx-config-map --from-file=nginx-config.conf
+kubectl create configmap nginx-config-map --from-file=k8s/nginx-config.conf
 
 REM Create TLS Secret for Nginx using provided certificates
 kubectl create secret tls tls-certs-secret --cert=nginx-server.crt --key=nginx-server.key
 
 REM Deploy Kubernetes Manifests for Iris app, services, and Nginx
-kubectl apply -f iris-app.yaml -f iris-service.yaml -f nginx.yaml -f nginx-service.yaml
+kubectl apply -f k8s/iris-app.yaml -f k8s/iris-service.yaml -f k8s/nginx.yaml -f k8s/nginx-service.yaml
 
 REM Wait for resources to become available
 echo Waiting for Iris deployment...
